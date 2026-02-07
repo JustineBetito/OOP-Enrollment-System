abeff92f22125fa8fcdc908af9b4f991bc0baf6a
Justine Danielle L. Betito <justinebetito17@gmail.com>
2026-02-07 11:14:02 +0800

    Third commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Header 1
+## Header 2
+### Header 3
+
+---
+This is a simple **markdown**
+
+List
+- list 1
+- list 2
+
+1. first number
+2. Second number
+
+Links: [Click here](https://github.com/JustineBetito/OOP-Enrollment-System)
+
+Image: (![Scan.png](src/main/resources/Images/Scan.png))
+
+# Title
+
+---
+
+**Author**: Justine Danielle L. Betito
+
+**1. Decription**
+
+[![Scan.png](src/main/resources/Images/Scan.png)]
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,4 +1,55 @@
 package org.example;
 
 public class Student {
+
+    private String studentID;
+    private String studentName;
+    private String program;
+
+    public Student(){
+
+    }
+
+    public Student(String studentID, String studentName, String program){
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.program = program;
+    }
+
+    public String getStudentID(){
+        return studentID;
+    }
+
+    public void setStudentID(String studentID){
+
+        this.studentID = studentID;
+    }
+
+    public String getStudentName(){
+
+        return studentName;
+    }
+
+    public void setStudentName(String studentName){
+
+        this.studentName = studentName;
+    }
+
+    public String getProgram(){
+
+        return program;
+    }
+
+    public void setProgram(String program){
+
+        this.program = program;
+    }
+
+    public void display(){
+        System.out.printf("\nStudent Name: %s\nStudent ID %s\nProgram %s", getStudentName(),getStudentID(), getProgram());
+
+    }
+
+
 }
+
