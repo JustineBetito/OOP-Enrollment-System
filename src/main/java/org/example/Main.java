be4567f3deda8016ca1cb5e56aae7f2ed07a983e
Justine Danielle L. Betito <justinebetito17@gmail.com>
2026-03-07 11:58:46 +0800

    march7v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,44 @@
 package org.example;
 
+import org.example.model.Course;
+import org.example.model.Student;
+import org.example.service.StudentRegistration;
+
 public class Main {
     public static void main(String[] args) {
 
 
+        // Create
+        StudentRegistration.addStudent(new Student("2024384681", "Justine Danielle L. Betito", "BSIT "));
+        studentEnrollment.addStudent(new Student("2024681384", "Danielle Justine Lopez", "BSIT"));
+
+        courseEnrollment.addCourse(new Course("0001", "Inteprog", "BSIT"));
+        courseEnrollment.addCourse(new Course("0002", "IT Proma", "BSIT"));
+
+
+        // Display
+        studentEnrollment.displayAll();
+        courseEnrollment.displayAll();
+
+        // Update
+        Student updatedInfo = new Student("2024384681", "John Doe", "BSCS");
+        studentEnrollment.updateStudent(updatedInfo);
+        studentEnrollment.displayAll();
+
+
+        Course updatedCourse = new Course("0002", "Pathfi", "BSIT");
+        courseEnrollment.updateCourse(updatedCourse);
+        courseEnrollment.displayAll();
+
+        // Remove
+
+        Student removeInfo = new Student("2024384681", "", "");
+        studentEnrollment.deleteStudent(removeInfo);
+        studentEnrollment.displayAll();
+
+        Course removeCourse = new Course("0002", "", "");
+        courseEnrollment.deleteCourse(removeCourse);
+        courseEnrollment.displayAll();
 
     }
 }
